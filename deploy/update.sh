9a465d464f6b63bdb5ad7af2b6e0611e7465a4aa
Erick <erick.oziel@gmail.com>
2020-03-01 23:42:46 +0100

    Updating update.sh to include installing new requirements

--- deploy/update.sh
+++ deploy/update.sh
@@ -5,6 +5,7 @@ set -e
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 git pull
+$PROJECT_BASE_PATH/env/bin/pip install -r requirements.txt
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart profiles_api
